862433b6579f344466e564fc754f5312bc40e2f4
kowais915 <kowais915@gmail.com>
2023-07-28 21:47:28 +0500

    :recycle: Added a main component

---
+++ components/Main.js
@@ -0,0 +1,10 @@
+const Main = () => {
+    return ( 
+        <>
+            <h1>This is the main component</h1>
+            {/* // * place child components here */}
+        </>
+     );
+}
+ 
+export default Main;
