f65e0a0d2f9f71349f6de2efb6003ee3744b3b64
kowais915 <kowais915@gmail.com>
2023-07-29 04:04:13 +0500

    :construction: Working on the home page

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,16 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
+// * importing components
+import HomePage from '../components/HomePage'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <h1>The coolest app ever built during a hackathon</h1>
+      <HomePage/>
     </>
   )
 }
