b3d2870445c480ba9329bc2a75e44a5ca16be923
kowais915 <kowais915@gmail.com>
2023-08-01 01:52:30 +0500

    :iphone: Made the navbar responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,13 @@
 const Navbar = () => {
     return (
-        <nav className="flex flex-col bg-gradient-to-r from-teal-500 to-pink-500 sm:flex-row sm:justify-between md:justify-between">
+        <nav className="flex flex-col items-center bg-gradient-to-r from-teal-500 to-pink-500 sm:flex-row sm:justify-between md:justify-between">
             <div className="title">
             <h1 className="text-2xl p-5 mt-2 text-white ">FreeFoodster</h1>
             </div>
 
-         <div className="flex flex-col sm:flex-row">
+         <div className="flex flex-col  items-center sm:flex-row">
              
-             <div className="card m-5 hover:bg-sky-700 hover:text-white p-3 rounded-md cursor-pointer ">
+             <div className="card m-1  hover:bg-sky-700 hover:text-white p-3 rounded-md cursor-pointer ">
                  <p>About</p>
              
              </div>
